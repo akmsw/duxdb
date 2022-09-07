5bbc44846db5eae58b760e84e5d33a85c1a7c9fb
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:07:46 -0300

    Update duxdbqueries.sql

--- duxdbqueries.sql
+++ duxdbqueries.sql
@@ -11,3 +11,20 @@ where
 	rubro = "librería"
 and
 	fecha_creacion = date(now());
+
+
+
+
+
+/* Monto total vendido por cliente */
+select
+	c.nombre as nombre_cliente,
+	c.apellido as apellido_cliente,
+	v.cantidad * v.precio_unitario as monto_total
+from
+	venta as v
+join
+	cliente as c
+	using(id_cliente)
+group by
+	id_cliente;
