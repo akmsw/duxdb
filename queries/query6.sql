76e82078b0433154952e94f19adfed14d653aaed
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 20:14:52 -0300

    Create query6.sql

---
+++ queries/query6.sql
@@ -0,0 +1,31 @@
+select
+	r.rubro
+from
+	rubro as r
+left join
+	producto as p
+	using(id_rubro)
+left join
+	venta as v
+	on codigo = codigo_producto
+where
+	v.fecha < date(now() - interval 2 month)
+    and r.rubro not in
+    (
+      select
+        r.rubro
+      from
+        rubro as r
+      left join
+        producto as p
+        using(id_rubro)
+      left join
+        venta as v
+        on codigo = codigo_producto
+      where
+        v.fecha >= date(now() - interval 2 month)
+      group by
+        id_rubro
+    )
+group by
+	id_rubro
