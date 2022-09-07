5afdc6754881295424d37b4cf9af196d406e0d28
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:20:57 -0300

    Create query1.sql

---
+++ query1.sql
@@ -0,0 +1,13 @@
+/* Todos los productos del rubro "librería" creados hoy */
+select
+	p.nombre as nombre_producto,
+	p.fecha_creacion,
+	r.rubro
+from
+	producto as p
+join
+	rubro as r
+where
+	rubro = "librería"
+and
+	fecha_creacion = date(now());
