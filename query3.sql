5c8b38e4d496757aabd227cd2ae1b2ff363ae1cc
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:59:26 -0300

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,12 +1,12 @@
 /* Cantidad de ventas por producto */
 select
 	p.nombre as nombre_producto,
-  count(id_venta) as cantidad_ventas
+	count(id_venta) as cantidad_ventas
 from
-	venta as v
-join
 	producto as p
+left join
+	venta as v
 on
-	codigo_producto = codigo
+	codigo = codigo_producto
 group by
 	nombre_producto;
