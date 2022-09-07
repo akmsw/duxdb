d2472c8827d15eb34c720e9ca05fdb21081a84ef
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 19:21:41 -0300

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -2,11 +2,11 @@
 select
 	c.nombre as nombre_cliente,
 	c.apellido as apellido_cliente,
-	v.cantidad * v.precio_unitario as monto_total
+	ifnull(v.cantidad * v.precio_unitario, 0) as monto_total
 from
-	venta as v
-join
 	cliente as c
+left join
+	venta as v
 	using(id_cliente)
 group by
 	id_cliente;
