1850d16ac71d447556224253f378db7f8be6a406
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:09:15 -0300

    Update duxdbqueries.sql

--- duxdbqueries.sql
+++ duxdbqueries.sql
@@ -36,7 +36,7 @@ group by
 /* Cantidad de ventas por producto */
 select
 	p.nombre as nombre_producto,
-    count(id_venta) as cantidad_ventas
+  count(id_venta) as cantidad_ventas
 from
 	venta as v
 join
@@ -45,3 +45,28 @@ on
 	codigo_producto = codigo
 group by
 	id_venta;
+
+
+
+
+/* Cantidad de productos comprados por cliente en el mes actual */
+select
+	c.nombre as nombre_cliente,
+	c.apellido as apellido_cliente,
+	count(*) as compras_este_mes
+from
+	(
+		select
+			id_cliente,
+			id_venta,
+			fecha
+		from
+			venta
+		where
+			month(fecha) = month(now())
+	) ventas_mes_actual
+join
+	cliente as c
+	using(id_cliente)
+group by
+	id_cliente;
