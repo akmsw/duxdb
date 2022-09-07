28ff9cb9d3330b8ed15741e3f795de2496776611
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:31:10 -0300

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,5 +1,5 @@
 /* Todos los productos del rubro "librería" creados hoy */
-select distinct
+select
 	p.nombre as nombre_producto,
 	p.fecha_creacion,
 	r.rubro
@@ -7,6 +7,7 @@ from
 	producto as p
 join
 	rubro as r
+	using(id_rubro)
 where
 	rubro = "librería"
 and
