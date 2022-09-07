497ab65a309362055b43801209546fb3c1df5e6b
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-09-07 18:08:25 -0300

    Update duxdbqueries.sql

--- duxdbqueries.sql
+++ duxdbqueries.sql
@@ -28,3 +28,20 @@ join
 	using(id_cliente)
 group by
 	id_cliente;
+
+
+
+
+
+/* Cantidad de ventas por producto */
+select
+	p.nombre as nombre_producto,
+    count(id_venta) as cantidad_ventas
+from
+	venta as v
+join
+	producto as p
+on
+	codigo_producto = codigo
+group by
+	id_venta;
